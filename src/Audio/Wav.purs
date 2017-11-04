1f424719e1dbf5ace913c23ad1cc0dfd9e8c2719
Matthew Leon <ml@matthewleon.com>
2017-11-04 17:59:48 -0400

    subchunk1 decoding

--- src/Audio/Wav.purs
+++ src/Audio/Wav.purs
@@ -12,53 +12,71 @@ import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 
 type Wav =
-  { sampleRate  :: Int
+  { metadata    :: WavMetadata
   , channelData :: Array TA.Float32Array
+  }
+
+-- TODO: user UInt
+type WavMetadata =
+  { numChannels   :: Int
+  , sampleRate    :: Int
+  , byteRate      :: Int
+  , blockAlign    :: Int
+  , bitsPerSample :: Int
   }
 
 decode :: AB.ArrayBuffer -> Either String Wav
 decode ab =
   let dv = DV.fromArrayBuffer ab
   in  case runDecoder (runExceptT wavDecoder) dv 0 of
-      Just (Tuple _ (Right _)) -> Right {sampleRate: 0, channelData: []}
+      Just (Tuple _ (Right _)) -> Left "TODO"
       Just (Tuple _ l)         -> l
       Nothing                  -> Left "WAV parse error"
 
 -- http://soundfile.sapp.org/doc/WaveFormat/
 wavDecoder :: ExceptT String Decoder Wav
-wavDecoder =
-  let throw  = except <<< Left
-      get16  = lift getUint16le
-      get32  = lift getUint32le
-      getStr = lift $ getASCIIString 4
-  in do
-    -- https://en.wikipedia.org/wiki/FourCC
-    fourCC <- getStr
-    unless (fourCC == "RIFF") $
-      throw ("Invalid WAV file fourCC: " <> fourCC)
+wavDecoder = do
+  -- https://en.wikipedia.org/wiki/FourCC
+  checkStr4 "RIFF" "Invalid WAV file fourCC: "
+  lift $ skipBytes 4 -- ChunkSize... unnecessary?
+  checkStr4 "WAVE" "Invalid WAV format: "
+  metadata <- decodeSubchunk1
+  pure {metadata: metadata, channelData: []}
 
-    lift $ skipBytes 4 -- ChunkSize... unnecessary?
+  where
+    getu16  = lift getUint16le
+    getu32  = lift getUint32le
+    getStr4 = lift $ getASCIIString 4
 
-    format <- getStr
-    unless (format == "WAVE") $
-      throw ("Invalid WAV format: " <> format)
+    checkEq
+      :: forall a
+       . Eq a
+      => ExceptT String Decoder a
+      -> a
+      -> (a -> String)
+      -> ExceptT String Decoder Unit
+    checkEq getter shouldEq mkErr = do
+      got <- getter
+      unless (got == shouldEq) $ except <<< Left $ mkErr got
 
-    subChunk1Id <- getStr
-    unless (subChunk1Id == "fmt ") $
-      throw ("Invalid WAV subchunk1 id: " <> subChunk1Id)
-    
-    subChunk1Size <- get32
-    unless (subChunk1Size == 16) $
-      throw ("Invalid subchunk1 size: " <> show subChunk1Size)
+    checkStr4 shouldEq errStr = checkEq getStr4 shouldEq (errStr <> _)
 
-    audioFormat <- get16
-    unless (audioFormat == 1) $
-      throw ("Invalid audio format (only PCM supported): " <> show audioFormat)
+    decodeSubchunk1 = do
+      checkStr4 "fmt " "Invalid WAV subchunk1 id: "
+      checkEq getu32 16 \size -> "Invalid subchunk1 size: " <> show size
+      checkEq getu16 1 \fmt -> "Invalid audio format: " <> show fmt
 
-    numChannels   <- get16
-    sampleRate    <- get32
-    byteRate      <- get32
-    blockAlign    <- get16
-    bitsPerSample <- get16
+      numChannels   <- getu16
+      sampleRate    <- getu32
+      byteRate      <- getu32
+      blockAlign    <- getu16
+      bitsPerSample <- getu16
 
-    pure {sampleRate: sampleRate, channelData: []}
+      pure {
+        numChannels:   numChannels
+      , sampleRate:    sampleRate
+      , byteRate:      byteRate
+      , blockAlign:    blockAlign
+      , bitsPerSample: bitsPerSample
+      }
+
