3bf23ac5398d4432165d8c6373e0ade39b0f6487
Matthew Leon <ml@matthewleon.com>
2017-11-17 19:22:41 -0500

    minibench parsing

--- test/Main.purs
+++ test/Main.purs
@@ -9,10 +9,12 @@ import Control.Monad.Eff.Exception (EXCEPTION)
 import Node.Buffer (BUFFER, toArrayBuffer)
 import Node.FS (FS)
 import Node.FS.Sync (readFile)
+import Performance.Minibench (bench)
 
 main :: forall e. Eff (buffer :: BUFFER, fs :: FS, console :: CONSOLE, exception :: EXCEPTION | e) Unit
 main = do
   gongBuffer <- readFile "test/gong.wav"
   gongAB <- toArrayBuffer gongBuffer
+  bench \_ -> Wav.decode gongAB
   let gongWav = Wav.decode gongAB
   logShow $ Wav.toString <$> gongWav
